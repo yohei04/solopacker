ee4f2aa31412c1194f991a9667c2bf32d587ef2e
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-07 11:52:49 +0900

    アセットエラー修正試した(エラーのまま)

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module Solopacker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.enabled = false
+
+    config.generators do |g|
+      g.assets false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

6727501b16187a19cbf454d29528241199325da8
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-10 01:14:33 +0900

    デフォルトのテンプレートエンジンをslimに変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Solopacker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.generators.template_engine = :slim  
 
     config.generators do |g|
       g.stylesheets false

b0b4c62507d7b1a51c442ce921e339b76898c962
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-12 16:54:40 +0900

    deviseのルーティング変更 #10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
   get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

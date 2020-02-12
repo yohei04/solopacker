c3d253e2d516b025403002b8197f8d794e6c4033
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-12 17:53:15 +0900

    サインアップ機能の実装 #10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :user_name, presence: true, uniqueness: true, length: { maximum: 15 }
 end

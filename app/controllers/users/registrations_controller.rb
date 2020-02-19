0ac003f24dd73d7bcebf48459d910fc58050ed12
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-19 11:53:39 +0900

    root_pathとHome画面のもろもろのリンク先を変更 #14

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,11 +50,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(_resource)
-    static_pages_home_path
+    root_path
   end
 
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(_resource)
-    static_pages_home_path
+    root_path
   end
 end

7517858542cf2450b110a3f62a746f3ef8cc27b5
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-22 11:14:04 +0900

    profilesコントローラーとviewの作成 #18

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/new'
+  get 'profiles/edit'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'

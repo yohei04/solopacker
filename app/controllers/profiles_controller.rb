352f1d4993a8e73372547087bb39b6b5d16d808a
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-02-22 17:32:58 +0900

    Profileの画像UP以外のバック実装 #18

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,17 @@
 class ProfilesController < ApplicationController
-  def new
-  end
 
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to root_path, notice: "Save profile successfully."
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:date_of_birth, :gender, :origin, :current_city, :language_1, :language_2, :language_3, :introduce, :image)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'profiles/new'
-  get 'profiles/edit'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   
   root 'static_pages#home'
+  resources :profiles, only: [:edit, :update]
 end

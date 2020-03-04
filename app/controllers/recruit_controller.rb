4694e4125e820ffc9c10025095e3e070ff32be66
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-03-04 21:35:33 +0900

    Recruitモデルのcreateアクション実装 #23

--- app/controllers/recruit_controller.rb
+++ app/controllers/recruit_controller.rb
@@ -1,23 +1,31 @@
 class RecruitController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def new
     @recruit = Recruit.new
   end
 
   def create
+    @recruit = current_user.recruits.build(recruit_params)
+    if @recruit.save
+      flash[:notice] = 'Recruit created!'
+      redirect_to root_path
+    else
+      render new_recruit_path
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
+
+  private
+
+    def recruit_params
+      params.require(:recruit).permit(:date_time, :hour, :country, :city, :title, :content)
+    end
 end

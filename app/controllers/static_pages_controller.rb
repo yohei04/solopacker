3e533fa7b6c2e4277469dfc491819c6152d0a856
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-03-05 21:04:12 +0900

    home画面にページネーションと検索機能追加 #23

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @recruits = Recruit.all.recent
+    # @recruits = Recruit.all.recent
+    @q = Recruit.ransack(params[:q])
+    @recruits = @q.result(distinct: true).page(params[:page]).per(24)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,16 @@ User.create!(name: 'foobar',
               #  image: Rack::Test::UploadedFile.new(Rails.root.join("db/fixtures/images/img#{n}.jpg"))
               )
 end
+
+
+users = User.recent.take(6)
+10.times do
+  users.each { |user| user.recruits.create!(
+    date_time: Faker::Time.between(DateTime.now, DateTime.now + 10),
+    hour: [*1..24].sample,
+    country: Faker::Address.country_code,
+    city: Faker::Address.city,
+    title: Faker::Book.title,
+    content: [Faker::Matz.quote, Faker::OnePiece.quote].sample
+  ) }
+end

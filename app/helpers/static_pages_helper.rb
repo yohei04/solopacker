41a044d7aa71e33180e23ce7bc4ab5ff6c82fb31
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-03-23 02:24:38 +0900

    Recruit一覧のタイトル用のメソッド作成 #37

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+  def recruits_title
+    search_country_code = params[:q][:country_cont]
+    if search_country_code.present?
+      "Recruits in #{country_name(search_country_code)}"
+    else
+      'All Recruits'
+    end
+  end
 end

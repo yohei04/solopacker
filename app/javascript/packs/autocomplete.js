01549b4c29682825122d523fac7a538f51295d78
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-06 14:42:06 +0900

    autocomplete.jsからturbolinks削除 #39

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,31 +1,29 @@
-$(document).on('turbolinks:load', function () {
-  function initializeAutocomplete(id) {
-    let element = document.getElementById(id);
-    if (element) {
-      let autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
-      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-    }
+function initializeAutocomplete(id) {
+  let element = document.getElementById(id);
+  if (element) {
+    let autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
   }
+}
 
-  function onPlaceChanged() {
-    let place = this.getPlace();
+function onPlaceChanged() {
+  let place = this.getPlace();
 
-    // console.log(place);  // Uncomment this line to view the full object returned by Google API.     
+  // console.log(place);  // Uncomment this line to view the full object returned by Google API.     
 
-    for (let i in place.address_components) {
-      let component = place.address_components[i];
-      for (let j in component.types) {  // Some types are ["country", "political"]
-        let type_element = document.getElementById(component.types[j]);
-        if (type_element) {
-          type_element.value = component.short_name;
-        }
+  for (let i in place.address_components) {
+    let component = place.address_components[i];
+    for (let j in component.types) {  // Some types are ["country", "political"]
+      let type_element = document.getElementById(component.types[j]);
+      if (type_element) {
+        type_element.value = component.short_name;
       }
     }
   }
-  google.maps.event.addDomListener(window, 'load', function () {
-    initializeAutocomplete('autocomplete_address');
-  });
-}); 
+}
+google.maps.event.addDomListener(window, 'load', function () {
+  initializeAutocomplete('autocomplete_address');
+});
 
 // cityを空にする
 // document.getElementById('autocomplete_address').value = "";

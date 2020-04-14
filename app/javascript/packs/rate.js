7a711fa64f2239e2a034b9a3785771b2b57637ba
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-15 00:02:31 +0900

    ベース通貨変更に対応 #41

--- app/javascript/packs/rate.js
+++ app/javascript/packs/rate.js
@@ -8,63 +8,38 @@ $(function () {
   // レートと通貨のjson
   var $rate_json = $('#rate_json').data('resource')
 
-  
 
-  //計算結果
-  var inputBaseRateResult = function ($inputTargetRate, $targetRate) {
-    return $inputBaseRate.val(Math.round($inputTargetRate.val() / $targetRate.val() * 1000) / 1000);
+  //計算結果を算出する関数
+  var inputBaseRateResult = function ($inputTargetRate, $baseRate, $targetRate) {
+    return $inputBaseRate.val(Math.round($inputTargetRate.val() * $baseRate.val() / $targetRate.val() * 1000) / 1000);
   };
-  var inputTargetRateResult = function ($inputBaseRate, $targetRate) {
-    return $inputTargetRate.val(Math.round($inputBaseRate.val() * $targetRate.val() * 1000) / 1000);
+  var inputTargetRateResult = function ($inputBaseRate, $baseRate, $targetRate) {
+    return $inputTargetRate.val(Math.round($inputBaseRate.val() / $baseRate.val() * $targetRate.val() * 1000) / 1000);
   };
 
   // ベースレート入力
   $inputBaseRate.on('input', function () {
-    inputTargetRateResult($inputBaseRate, $targetRate);
+    inputTargetRateResult($inputBaseRate, $baseRate, $targetRate);
   });
 
   // ターゲットレート入力
   $inputTargetRate.on('input', function () {
-    inputBaseRateResult($inputTargetRate, $targetRate);
+    inputBaseRateResult($inputTargetRate, $baseRate, $targetRate);
   });
 
-  // return $result.val(Math.round($baseRate.val() * $targetRate.val() * 1000) / 1000)
-  
+  // ベース通貨セレクト
+  $baseCurrency.change(function () {
+    var selectedBaseCurrency = $baseCurrency.val();
+    selectedBaseCurrencyRate = $rate_json[`${selectedBaseCurrency}`]
+    $baseRate.val(selectedBaseCurrencyRate);
+    inputTargetRateResult($inputBaseRate, $baseRate, $targetRate);
+  });
 
   // ターゲット通貨セレクト
   $targetCurrency.change(function() {
     var selectedTargetCurrency = $targetCurrency.val();
     selectedTargetCurrencyRate = $rate_json[`${selectedTargetCurrency}`]
     $targetRate.val(selectedTargetCurrencyRate);
-    inputTargetRateResult($inputBaseRate, $targetRate);
-  });
-
-  // ターゲット通貨セレクト
-  $baseCurrency.change(function() {
-    var selectedBaseCurrency = $baseCurrency.val();
-    selectedBaseCurrencyRate = $rate_json[`${selectedBaseCurrency}`]
-    $baseRate.val(selectedBaseCurrencyRate);
-    inputBaseRateResult($inputBaseRate, $targetRate);
+    inputBaseRateResult($inputTargetRate, $baseRate, $targetRate);
   });
 });
-
-
-
-// let baseRate = document.getElementById("base_rate");
-// let targetRate = document.getElementById("target_rate");
-
-
-// console.log(baseRate)
-// console.log(baseRate)
-
-// function OnButtonClick() {
-//   target = document.getElementById("output");
-//   target.innerHTML = "Penguin";
-// }
-
-// document.getElementById("button").onclick = function () {
-//   document.getElementById("target_rate").value = document.getElementById("base_rate").value * targetRate.value
-//   // document.getElementById("output").innerHTML = document.getElementById("base_rate").value * targetRate.value
-  
-// };
-

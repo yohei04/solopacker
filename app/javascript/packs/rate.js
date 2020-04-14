70a7eb22ec9a040d8e667978cd2578719d0ec9c2
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-14 15:23:02 +0900

    rateのデータをdate属性でHTMLに渡す #41

--- app/javascript/packs/rate.js
+++ app/javascript/packs/rate.js
@@ -2,12 +2,13 @@ $(function () {
   var $baseRate = $('#base_rate');
   var $targetRate = $('#target_rate');
   var $result = $('#result');
+  var $rate_json = $('#rate_json').data('resource')
+  console.log($rate_json["JPY"])
   $result.val($baseRate.val() * $targetRate.val())
   $baseRate.on('input', function (event) {
     var value = $baseRate.val();
     $result.val(Math.round(value * $targetRate.val() * 1000) / 1000);
   });
-  console.log($targetRate)
 });
 
 

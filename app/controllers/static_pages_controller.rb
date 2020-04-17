dd6419fb93ad81f97500e8eae789b735dd70f038
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-17 09:23:24 +0900

    コメントアウト削除 #41

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,11 +7,5 @@ class StaticPagesController < ApplicationController
   end
 
   def rate
-    # include RateApi
-    # @recruits = current_user.recruits
-    # @base_currency = currency(current_user.origin)
-    # @target_currency = currency(current_user.current_country)
-    # @exchange_rate = exchange_rate(@base_currency, @target_currency)
-    # redirect_to rate_path
   end
 end

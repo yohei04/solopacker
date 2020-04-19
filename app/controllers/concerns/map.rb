00a734d05f6a8dbde6a1a4736e4c55c6555ea574
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-19 14:10:15 +0900

    mapで過去の募集が表示されないよう実装 #47

--- app/controllers/concerns/map.rb
+++ app/controllers/concerns/map.rb
@@ -11,8 +11,8 @@ module Map
   end
 
   # 全ての募集のjson
-  def recruits_all_json
-    Recruit.all.includes(user: { image_attachment: :blob }).map do |r|
+  def feature_recruits_json
+    Recruit.happen_recent.includes(user: { image_attachment: :blob }).map do |r|
       if(r.user.image.attached?)
         user_image_path = Rails.application.routes.url_helpers.rails_blob_path(r.user.image, only_path: true)
       else

e1ced4ad939f806cb74001abca37c251c80c7d23
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-22 20:55:30 +0900

    ピンでも国を選べるよう実装 #47

--- app/javascript/packs/recruit_serch.js
+++ app/javascript/packs/recruit_serch.js
@@ -3,5 +3,12 @@ $(function () {
   $('#q_country_cont').change(function () {
     $("form").find("button[type='submit']").click();
   });
+  $('.all_countries').click(function () {
+    $('#q_country_cont').val('');
+    $("form").find("button[type='submit']").click();
+  })
+  $('.pin5').click(function () {
+    $('#q_country_cont').val("JP");
+    $("form").find("button[type='submit']").click();
+  })
 });
-

33a0fc372f4f7897206a967f7ea0871a35012f4b
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-23 17:28:35 +0900

    ユーザーアイコン画像変更 #47

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ User.create!(name: 'foobar',
 json = ActiveSupport::JSON.decode(File.read('db/fixtures/country/cities_countries.json'))
 all_cities = json.map { |hash| hash["city"] }
 
-20.times do |n|
+30.times do |n|
   User.create!(name: Faker::DragonBall.character,
                user_name: Faker::Name.first_name,
                email: "foobar#{n-1}@example.com",
@@ -36,7 +36,7 @@ all_cities = json.map { |hash| hash["city"] }
                language_2: Faker::Nation.language,
                language_3: Faker::Nation.language,
                introduce: [Faker::Matz.quote, Faker::OnePiece.quote].sample,
-              #  image: Rack::Test::UploadedFile.new(Rails.root.join("db/fixtures/images/img#{n+1}.jpg"))
+               image: Rack::Test::UploadedFile.new(Rails.root.join("db/fixtures/images/img#{n+1}.jpg"))
               )
 end
 
@@ -72,7 +72,7 @@ sample_users = User.all.sample(21)
   }
 end
 
-recruits = Recruit.order(created_at: :desc).take(15)
+recruits = Recruit.order(created_at: :desc).take(10)
 5.times do
   recruits.each { |recruit| recruit.comments.create!(
     content: [Faker::Movie.quote].sample,

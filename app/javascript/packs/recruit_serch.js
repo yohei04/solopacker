f069c42275cd6e09990145ca40007a7893de0808
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-04-23 10:51:42 +0900

    homeでの国検索を関数化 #47

--- app/javascript/packs/recruit_serch.js
+++ app/javascript/packs/recruit_serch.js
@@ -1,14 +1,16 @@
 // 国を選択すると自動で検索
 $(function () {
+  function searchCountry(pin, country) {
+    $(pin).click(function () {
+      $('#q_country_cont').val(country);
+      $("form").find("button[type='submit']").click();
+    });
+  };
+
   $('#q_country_cont').change(function () {
     $("form").find("button[type='submit']").click();
   });
-  $('.all_countries').click(function () {
-    $('#q_country_cont').val('');
-    $("form").find("button[type='submit']").click();
-  })
-  $('.pin5').click(function () {
-    $('#q_country_cont').val("JP");
-    $("form").find("button[type='submit']").click();
-  })
+  searchCountry('.all_countries', '');
+  searchCountry('.pin5', 'JP');
 });
+
